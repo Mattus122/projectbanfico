ceb8a446e42d173adb50564a6133107eff285b4c
Mattus122 <mananmatta0105@gmail.com>
2024-06-25 18:13:42 +0530

    User Registration branch

---
+++ src/main/java/com/example/DemoProject/controlleradvice/ErrorHandler.java
@@ -0,0 +1,39 @@
+package com.example.DemoProject.controlleradvice;
+
+import com.example.DemoProject.dto.ErrorObject;
+import com.example.DemoProject.exception.InvalidRoleException;
+import com.example.DemoProject.exception.UnauthorizedException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.OffsetDateTime;
+
+@ControllerAdvice
+public class ErrorHandler {
+//    @ExceptionHandler(UnauthorizedException.class)
+//    public ResponseEntity<ErrorObject> Excetion1(UnauthorizedException e){
+//        ErrorObject er= ErrorObject.builder().offsetDateTime(OffsetDateTime.now()).message("Unauthorized Access").build();
+//
+//        return new ResponseEntity<>(er , HttpStatus.UNAUTHORIZED);
+//    }
+//        @ExceptionHandler(ConflictException.class)
+//        public ResponseEntity<ErrorObject> Exception2(ConflictException e){
+//            ErrorObject er = ErrorObject.builder().offsetDateTime(OffsetDateTime.now()).message("Role Not Found").build();
+//            return new ResponseEntity<>(er , HttpStatus.CONFLICT);
+//        }
+    @ExceptionHandler(UnauthorizedException.class)
+    public ResponseEntity<ErrorObject> Exception3(UnauthorizedException e){
+        ErrorObject er = ErrorObject.builder().offsetDateTime(OffsetDateTime.now()).message("Role Mismatch").code(e.getCode()).build();
+        return new ResponseEntity<>(er , HttpStatus.FORBIDDEN);
+    }
+    @ExceptionHandler(InvalidRoleException.class)
+    public ResponseEntity<ErrorObject> handleInvalidRoleException(InvalidRoleException ex) {
+        ErrorObject er = ErrorObject.builder().offsetDateTime(OffsetDateTime.now()).message("Invalid Role").code(ex.getCode()).build();
+        return new ResponseEntity<>(er , HttpStatus.BAD_REQUEST);
+    }
+
+
+
+}
---
+++ src/main/java/com/example/DemoProject/dto/ErrorObject.java
@@ -0,0 +1,19 @@
+package com.example.DemoProject.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.OffsetDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ErrorObject {
+    private OffsetDateTime offsetDateTime;
+    private String message;
+
+    private String code;
+}
